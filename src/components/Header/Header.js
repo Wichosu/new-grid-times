7aca5c8a642db2931888bf2a4feba014f18e483b
Wicho <54341712+Wichosu@users.noreply.github.com>
2024-09-08 09:28:19 -0600

    Exercise 1 done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,15 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <Search />
+          <Menu />
+        </DesktopActionGroup>
         <Logo />
+        <LoginGroup>
+          <SubscribeButton>SUBSCRIBE</SubscribeButton>
+          <Link href='./'>Already a subscriber?</Link>
+        </LoginGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +47,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +71,48 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: space-between;
+  }
+`;
+
+const LoginGroup = styled(ActionGroup)`
+  flex-direction: column;
+  gap: 8px;
+  align-self: flex-end;
+  text-align: center;
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+  }
+`;
+
+const SubscribeButton = styled(Button)`
+  align-self: flex-end;
+`
+
+const Link = styled.a`
+  color: var(--color-gray-900);
+  font-style: italic;
+  font-weight: 400;
+  font-size: ${14 / 16}rem;
+  text-decoration: underline;
 `;
 
 export default Header;
